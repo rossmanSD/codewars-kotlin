6cb692eca724d8bb855d7d303c17219a82d8f739
Ross Manges <ross@sparkistic.com>
2021-05-25 18:35:03 -0700

    Introducing a table of test values to streamline my Kotest tests.

--- src/test/kotlin/net/rossmanges/codewars/PopulationGrowthTest.kt
+++ src/test/kotlin/net/rossmanges/codewars/PopulationGrowthTest.kt
@@ -1,20 +1,24 @@
 package net.rossmanges.codewars
 
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.inspectors.forExactly
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
 import io.kotest.matchers.shouldBe
 
 class PopulationGrowthTest : StringSpec({
     val pg = PopulationGrowth()
-    "fixed tests" {
-        pg.nbYear(1500, 5.0, 100, 5000) shouldBe 15
-        pg.nbYear(1500000, 2.5, 10000, 2000000) shouldBe 10
-    }
-
-    "null and zero growthPct tests" {
-        val inputs = listOf(0.0, null)
-        inputs.forEach {
-            pg.nbYear(1500000, it, 10000, 2000000) shouldBe 50
+    "table of inputs" {
+        table(
+            headers("p0", "growth", "aug", "goal", "expected years"),
+            row(1500, 5.0, 100, 5000, 15),
+            row(1500000, 2.5, 10000, 2000000, 10),
+            row(1500000, 0.0, 10000, 2000000, 50),
+            row(1500000, null, 10000, 2000000, 50)
+        ).forAll { p0: Int, growth: Double?, aug: Int, goal: Int, expectedYears: Int ->
+            pg.idiomaticNbYear(p0, growth, aug, goal) shouldBe expectedYears
+            pg.nbYear(p0, growth, aug, goal) shouldBe expectedYears
         }
     }
 })
